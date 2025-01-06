9a472f7917aae25c88943757ceb33575b1e9f262
wlsdks <wlsdks12@naver.com>
2025-01-07 00:15:06 +0900

    :sparkles: 조금 더 확실한 테스트를 위해 새로운 executor 추가

--- src/main/java/com/example/demo/config/AsyncConfig.java
+++ src/main/java/com/example/demo/config/AsyncConfig.java
@@ -3,8 +3,8 @@ package com.example.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.security.concurrent.DelegatingSecurityContextExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.security.concurrent.DelegatingSecurityContextExecutor;
 
 import java.util.concurrent.Executor;
 
@@ -34,6 +34,17 @@ public class AsyncConfig {
         executor.setThreadNamePrefix("SecurityAsync-");
         executor.initialize();
         return new DelegatingSecurityContextExecutor(executor.getThreadPoolExecutor());
+    }
+
+    @Bean(name = "smallAsyncExecutor")
+    public Executor smallAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(1); // 스레드풀 크기: 1
+        executor.setMaxPoolSize(1);  // 최대 스레드 개수: 1
+        executor.setQueueCapacity(5); // 작업 대기열 크기: 5
+        executor.setThreadNamePrefix("AsyncTest-");
+        executor.initialize();
+        return executor;
     }
 
 }
--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/api/**").permitAll()
+                            .requestMatchers("/test/small-async").permitAll()
                             .anyRequest().authenticated();
                 })
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
